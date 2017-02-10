2d27c9c9534c6f3c5cb568653fd327a7641f30a3
Hassan <hassan.essabir@gmail.com>
2017-02-10 12:09:36 +0100

    committed com.javatpoint.n.dynamicbinding package

---
+++ javatpoint/src/com/javatpoint/n/dynamicbinding/Dog.java
@@ -0,0 +1,14 @@
+package com.javatpoint.n.dynamicbinding;
+
+//Example of static binding
+class Dog {
+	private void eat() {
+		System.out.println("dog is eating...");
+	}
+
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		d1.eat();
+	}
+
+}
