d1afbd5ea96fd32075866332c7826ac4ac848528
Hassan <hassan.essabir@gmail.com>
2017-02-12 01:19:25 +0000

    Committed com.javatpoint.o.instanceofoperator package

---
+++ javatpoint/src/com/javatpoint/o/instanceofoperator/Dog1.java
@@ -0,0 +1,14 @@
+package com.javatpoint.o.instanceofoperator;
+
+class Animal {
+}
+
+class Dog1 extends Animal {
+
+	public static void main(String[] args) {
+		Dog1 d = new Dog1();
+		System.out.println(d instanceof Animal);// true
+
+	}
+
+}
