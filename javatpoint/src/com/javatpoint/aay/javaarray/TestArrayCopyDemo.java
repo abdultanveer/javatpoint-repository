4b94f475d5e586d44b3c6936b22f7d21f84d7019
Lucas Legname <lucas.legname@open-groupe.com>
2017-02-13 15:20:27 +0100

    committed com.javatpoint.aay.javaarray package

---
+++ javatpoint/src/com/javatpoint/aay/javaarray/TestArrayCopyDemo.java
@@ -0,0 +1,16 @@
+package com.javatpoint.aay.javaarray;
+
+class TestArrayCopyDemo {
+
+	public static void main(String[] args) {
+		char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a',
+				't', 'e', 'd' };
+		char[] copyTo = new char[7];
+
+		//System.out.println(new String(copyFrom));
+		System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+		System.out.println(new String(copyTo));
+
+	}
+
+}
