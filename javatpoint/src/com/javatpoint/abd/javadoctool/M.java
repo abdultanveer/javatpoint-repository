4b7def226088860f3556bf07f577b5a8bbdad5d9
Lucas Legname <lucas.legname@open-groupe.com>
2017-02-13 17:06:57 +0100

    committed com.javatpoint.abd.javadoctool package

---
+++ javatpoint/src/com/javatpoint/abd/javadoctool/M.java
@@ -0,0 +1,15 @@
+package com.javatpoint.abd.javadoctool;
+
+/** This class is a user-defined class that contains one methods cube. */
+public class M {
+
+	/** The cube method prints cube of the given number */
+	public static void cube(int n) {
+		System.out.println(n * n * n);
+	}
+	
+	public static void main(String[] arg){
+		cube(3);
+	}
+
+}
