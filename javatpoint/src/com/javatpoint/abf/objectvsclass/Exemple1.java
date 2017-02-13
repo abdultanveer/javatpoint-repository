b76957e4d05e0a62e70745cc1fc8094ad4c0b1e1
Lucas Legname <lucas.legname@open-groupe.com>
2017-02-13 17:22:30 +0100

    committed com.javatpoint.abf.objectvsclass package

---
+++ javatpoint/src/com/javatpoint/abf/objectvsclass/Exemple1.java
@@ -0,0 +1,36 @@
+package com.javatpoint.abf.objectvsclass;
+
+public class Exemple1 {
+
+	public static void main(String[] args) {
+
+		// Difference between object and class
+
+		System.out.println("No.	Object");
+
+		// Object is an instance of a class.
+		// Object is a real world entity such as pen, laptop, mobile, bed,
+		// keyboard, mouse, chair etc.
+		// Object is a physical entity.
+		// Object is created through new keyword mainly e.g.
+		// Student s1=new Student();
+		// Object is created many times as per requirement.
+		// Object allocates memory when it is created.
+		// There are many ways to create object in java such as new keyword,
+		// newInstance() method, clone() method, factory method and
+		// deserialization.
+		//
+
+		System.out.println("Class");
+
+		// Class is a blueprint or template from which objects are created.
+		// Class is a group of similar objects.
+		// Class is a logical entity.
+		// Class is declared using class keyword e.g.
+		// class Student{}
+		// Class is declared once.
+		// Class doesn't allocated memory when it is created.
+		// There is only one way to define class in java using class keyword.
+	}
+
+}
