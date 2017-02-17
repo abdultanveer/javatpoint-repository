c23410b56a825ed21d31f90b9347fcf0e822cc36
Hassan Essabir <hassan.essabir@gmail.com>
2017-02-17 13:47:15 +0100

    committed com.javatpoint.abv.javacollection

---
+++ javatpoint/src/com/javatpoint/abv/javacollection/ListExample.java
@@ -0,0 +1,20 @@
+package com.javatpoint.abv.javacollection;
+
+//Java List Example
+import java.util.*;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Amit");
+		al.add("Vijay");
+		al.add("Kumar");
+		al.add(1, "Sachin");
+		System.out.println("Element at 2nd position: " + al.get(2));
+		for (String s : al) {
+			System.out.println(s);
+		}
+	}
+
+}
---
+++ javatpoint/src/com/javatpoint/abv/javacollection/TreeSetExample.java
@@ -0,0 +1,50 @@
+package com.javatpoint.abv.javacollection;
+
+import java.util.*;
+
+class Book5 implements Comparable<Book5> {
+	int id;
+	String name, author, publisher;
+	int quantity;
+
+	public Book5(int id, String name, String author, String publisher,
+			int quantity) {
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.publisher = publisher;
+		this.quantity = quantity;
+	}
+
+	public int compareTo(Book5 b) {
+		if (id > b.id) {
+			return 1;
+		} else if (id < b.id) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+}
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		Set<Book5> set = new TreeSet<Book5>();
+		// Creating Books
+		Book5 b1 = new Book5(121, "Let us C", "Yashwant Kanetkar", "BPB", 8);
+		Book5 b2 = new Book5(233, "Operating System", "Galvin", "Wiley", 6);
+		Book5 b3 = new Book5(101, "Data Communications & Networking",
+				"Forouzan", "Mc Graw Hill", 4);
+		// Adding Books to TreeSet
+		set.add(b1);
+		set.add(b2);
+		set.add(b3);
+		// Traversing TreeSet
+		for (Book5 b : set) {
+			System.out.println(b.id + " " + b.name + " " + b.author + " "
+					+ b.publisher + " " + b.quantity);
+		}
+	}
+
+}
